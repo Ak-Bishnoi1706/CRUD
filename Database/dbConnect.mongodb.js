99fc44de0baaf6c4b960df53c4ba7d512c90b449
Anita Bishnoi <anitakaliyana01@gmail.com>
2025-11-10 16:16:29 +0530

    Moved Frontend files to root

--- Database/dbConnect.mongodb.js
+++ Database/dbConnect.mongodb.js
@@ -8,3 +8,4 @@ mongoose.connect(process.env.DB_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log("X DB error: ", err.message));
 console.log("DB URI:", process.env.DB_URI);
+
